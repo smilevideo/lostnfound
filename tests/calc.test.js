97673909362610ac5cb7420e7865c15f3787898d
smilevideo <andrewk1216@gmail.com>
2020-01-23 17:58:03 -0500

    setup testing with jest

---
+++ tests/calc.test.js
@@ -0,0 +1,14 @@
+//Nearest mode:
+test('adds 1 + 2 to equal 3', () => {
+    expect(1).toEqual(1);
+});
+
+//Upper Limit mode:
+test('adds 1 + 2 to equal 3', () => {
+    expect(1).toEqual(1);
+});
+
+//Lower Limit mode:
+test('adds 1 + 2 to equal 3', () => {
+    expect(1).toEqual(1);
+});
