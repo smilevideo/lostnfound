8dcfe226e56dceee1bfb0abd5faf939a956c1333
smilevideo <andrewk1216@gmail.com>
2020-01-23 19:03:23 -0500

    a few tests

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,14 +1,19 @@
+import calc from "../components/calc";
+
+let targetBPM = 400;
+let songBPM = 150;
+
 //Nearest mode:
-test('adds 1 + 2 to equal 3', () => {
-    expect(1).toEqual(1);
+test('finds nearest multiplied bpm correctly', () => {
+    expect(calc(targetBPM, songBPM)).toEqual(2.75);
 });
 
 //Upper Limit mode:
-test('adds 1 + 2 to equal 3', () => {
-    expect(1).toEqual(1);
+test('finds greatest multiplied bpm less than limit correctly', () => {
+    expect(calc(targetBPM, songBPM)).toEqual(2.5);
 });
 
 //Lower Limit mode:
-test('adds 1 + 2 to equal 3', () => {
-    expect(1).toEqual(1);
+test('finds smallest multiplied bpm greater than limit correctly', () => {
+    expect(calc(targetBPM, songBPM)).toEqual(2.75);
 });
