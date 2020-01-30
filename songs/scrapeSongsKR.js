6bb6e8ca64dbd44ce9a5105402cbec9f4adb99a0
smilevideo <andrewk1216@gmail.com>
2020-01-29 21:53:32 -0500

    use maximum bpm for variable bpm songs in KR list

--- songs/scrapeSongsKR.js
+++ songs/scrapeSongsKR.js
@@ -5,21 +5,29 @@ const cheerio = require('cheerio');
 
 request('https://djmaxdb.com/4B/', (error, response, html) => {
     if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        
         const songs = [];
+
+        const $ = cheerio.load(html);
 
         $('#DB_TABLE tr').each((i, el) => {
            const row = $(el);
            const title = row.find('td:nth-child(2)').text();
-           const bpm = row.find('td:nth-last-child(2)').text();
+           let bpm = row.find('td:nth-last-child(2)').text();
+
+           //use maximum bpm for songs with bpm changes
+           if (bpm.includes('~')) {
+               bpm = bpm.split('~')[1].trim();
+           } 
+
+           bpm = parseInt(bpm, 10);
 
            songs.push({
                title: title,
-               BPM: parseInt(bpm, 10)
+               BPM: bpm
            });
         });
 
+        //sort songs by title alphabetically
         songs.sort((a, b) => {
             const titleA = a.title.toLowerCase();
             const titleB = b.title.toLowerCase();
@@ -29,6 +37,7 @@ request('https://djmaxdb.com/4B/', (error, response, html) => {
             else return 0;
         })
 
+        //write to file songsKR.json
         let songsJSON = JSON.stringify(songs, null, 2);
         fs.writeFileSync('./songsKR.json', songsJSON)
     }
