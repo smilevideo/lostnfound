4865d5acceca84e53cb9693ee1732f816e9b02f7
smilevideo <andrewk1216@gmail.com>
2020-02-04 19:41:43 -0500

    change favicon to moai

--- components/docHead.js
+++ components/docHead.js
@@ -3,10 +3,12 @@ import Head from 'next/head'
 const DocHead = () => (
     <Head>
         <title>Lost n' found</title>
-        <link rel="icon" href="/favicon.ico" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        
+
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
+        
+        <link rel='icon' href='/favicon.ico?' type='image/x-icon'/>
     </Head>
 )
 
